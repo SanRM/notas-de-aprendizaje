b27537a22ebcaf63bec9b0052e0280e67d8bc350
SanRM <santiagoromom95@gmail.com>
2024-06-18 16:02:39 -0500

    docs: deploy - title changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Notas de aprendizaje',
+  title: 'Notas de aprendizaje - TEST',
   tagline: 'Santiago Rodriguez Morales',
   favicon: 'img/favicon.ico',
 
